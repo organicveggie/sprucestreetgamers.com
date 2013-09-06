5e3ac9c731a5244bfcecf88e306f7473041d9fb6
Sean Laurent <organicveggie@gmail.com>
2013-09-05 22:23:05 -0700

    Fixed the page less redirects plugin.,

--- plugins/pageless_redirects.rb
+++ plugins/pageless_redirects.rb
@@ -64,7 +64,7 @@ module Jekyll
     end
 
     def process_yaml
-      file_path = @site.source + "/_redirects.yml"
+      file_path = File.join(@site.source, "/_redirects.yml")
       if File.exists?(file_path)
         YAML.load_file(file_path, :safe => true).each do | new_url, old_url |
           generate_aliases( old_url, new_url )
@@ -73,55 +73,65 @@ module Jekyll
     end
 
     def process_htaccess
-      file_path = @site.source + "/_redirects.htaccess"
+      file_path = File.join(@site.source, "/_redirects.htaccess")
       if File.exists?(file_path)
         # Read the file line by line pushing redirects to the redirects array
-        file = File.new(file_path, "r")
-        while (line = file.gets)
-          # Match the line against a regex, if it matches push it to the object
-          /^Redirect(\s+30[1237])?\s+(.+?)\s+(.+?)$/.match(line) { | matches |
-            generate_aliases( matches[3], matches[2])
-          }
+        File.open(file_path, "r") do |file|
+          while (line = file.gets)
+            # Match the line against a regex, if it matches push it to the object
+            /^Redirect(\s+30[1237])?\s+(.+?)\s+(.+?)$/.match(line) { | matches |
+              generate_aliases(matches[3], matches[2])
+            }
+          end
         end
-        file.close
       end
     end
 
     def process_json
-      file_path = @site.source + "/_redirects.json"
+      file_path = File.join(@site.source, "/_redirects.json")
       if File.exists?(file_path)
-        file = File.new(file_path, "r")
-        content = JSON.parse(file.read)
-        content.each do |a, b|
+        File.open(file_path, "r") do |file|
+          content = JSON.parse(file.read)
+          content.each do |a, b|
             generate_aliases(a, b)
+          end
         end
-        file.close
       end
+
+      # @site.static_files.each {|sf| puts sf.path}
     end
 
     def generate_aliases(destination_path, aliases)
-      alias_paths ||= Array.new
+      alias_paths = []
       alias_paths << aliases
       alias_paths.compact!
 
       alias_paths.flatten.each do |alias_path|
         alias_path = alias_path.to_s
 
-        alias_dir  = File.extname(alias_path).empty? ? alias_path : File.dirname(alias_path)
+        alias_dir  = File.dirname(alias_path) == "." ? "/" : File.dirname(alias_path)
         alias_file = File.extname(alias_path).empty? ? "index.html" : File.basename(alias_path)
 
-        fs_path_to_dir   = File.join(@site.dest, alias_dir)
+        fs_path_to_dir   = File.absolute_path(File.join(@site.dest, alias_dir))
         alias_index_path = File.join(alias_dir, alias_file)
 
         FileUtils.mkdir_p(fs_path_to_dir)
 
-        File.open(File.join(fs_path_to_dir, alias_file), 'w') do |file|
+        fs_alias_file = File.join(fs_path_to_dir, alias_file)
+        File.open(fs_alias_file, 'w') do |file|
           file.write(alias_template(destination_path))
         end
 
-        (alias_index_path.split('/').size + 1).times do |sections|
-          @site.static_files << Jekyll::PagelessRedirectFile.new(@site, @site.dest, alias_index_path.split('/')[0, sections].join('/'), '')
-        end
+        # alias_path_parts = alias_index_path.split('/')
+        # if alias_path_parts.size > 2
+        #   alias_path_parts.size.times do |sections|
+        #     temp_path = alias_path_parts[0, sections].join('/')
+        #     @site.static_files << Jekyll::PagelessRedirectFile.new(@site, @site.dest, temp_path, alias_path_parts[sections + 1])
+        #   end
+        # else
+        #   @site.static_files << Jekyll::PagelessRedirectFile.new(@site, @site.dest, "/", alias_path_parts[-1])
+        # end
+        @site.static_files << Jekyll::PagelessRedirectFile.new(@site, @site.dest, alias_dir, alias_file)
       end
     end
 
@@ -142,9 +152,9 @@ module Jekyll
   class PagelessRedirectFile < StaticFile
     require 'set'
 
-    def destination(dest)
-      File.join(dest, @dir)
-    end
+    # def destination(dest)
+    #   File.join(dest, @dir)
+    # end
 
     def modified?
       return false
